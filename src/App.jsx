72710c8a11810f7e06f07a7167d1ce1adaab53fc
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-17 15:57:49 +0100

    layout CategoryCarousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import CategoryCarousel from "./components/CategoryCarousel"
 import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <CategoryCarousel />
     </div>
   )
 }
---
+++ src/components/CategoryCarousel.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useRef } from 'react'
+
+import { BsPlugin } from 'react-icons/bs'
+import { AiFillCaretLeft } from 'react-icons/ai'
+import { AiFillCaretRight } from 'react-icons/ai'
+
+function CategoryCarousel() {
+
+    const ref = useRef(null)
+
+    const scroll = (scrollOffset) => {
+        ref.current.scrollLeft += scrollOffset
+    }
+    
+
+  return (
+    <div className=' flex items-center justify-center bg-slate-800'>
+        <button onClick={() => scroll(-150)} id='slideLeft' className='flex item-center justify-center p-2'>
+            <AiFillCaretLeft fontSize='1.5em' className='text-slate-500'/>
+        </button>
+        <div ref={ref} id='carouselContainer' className=' flex flex-row w-2/4 overflow-x-hidden scroll-smooth gap-4 p-3'>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
+                <BsPlugin />
+                <a href="">Electronics</a>
+            </div>
+        </div>
+        <button onClick={() => scroll(150)} id='slideRight' className='flex item-center justify-center p-2'>
+            <AiFillCaretRight fontSize='1.5em' className='text-slate-500'/>
+        </button>
+    </div>
+  )
+}
+
+export default CategoryCarousel
