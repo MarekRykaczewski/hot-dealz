7ff5730dd14ca252a06ff06a8e9f71f9f65a247d
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-21 11:53:54 +0100

    compose Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import CategoryCarousel from '../components/CategoryCarousel'
+import Tabs from '../components/Tabs'
+import DealCard from '../components/DealCard'
+import FooterNav from '../components/FooterNav'
+
+
+function Home() {
+
+  return (
+    <div>
+    <CategoryCarousel />
+    <div className="flex flex-col w-full gap-3 justify-center items-center">
+      <Tabs />
+      <DealCard />
+      <DealCard />
+      <DealCard />
+      <DealCard />
+      <FooterNav />
+    </div>
+    </div>
+  )
+}
+
+export default Home
