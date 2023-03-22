cc36c28f353c9db75c0888f8b87cd1776e296aa1
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-22 18:33:58 +0100

    add link to home (logo)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 import { AiFillFire } from 'react-icons/ai'
 import { BiUserCircle } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 function Nav({toggleSignInModal}) {
+
+  const { user } = UserAuth()
+
   return (
     <nav className=' flex items-center justify-between flex-wrap bg-slate-700 p-6'>
-        <div className="flex items-center flex-shrink-0 text-white mr-6 gap-1">
+        <Link to='/' className="flex items-center flex-shrink-0 text-white mr-6 gap-1">
             <AiFillFire fontSize='2em' color='orange'/>
             <span className="font-semibold text-xl tracking-tight">Hot Dealz</span>
-        </div>
+        </Link>
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div className="text-sm lg:flex-grow">
             <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-white mr-4">
@@ -23,8 +28,8 @@ function Nav({toggleSignInModal}) {
             </a>
             </div>
             <div className='flex gap-2'>
-                <BiUserCircle onClick={toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2em' />
-                <AiOutlinePlusCircle className=' cursor-pointer' color='white' fontSize='2em'/>
+                {user ? <BiUserCircle onClick={toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2.5em' /> : <BiUserCircle onClick={toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2.5em' />}
+                <AiOutlinePlusCircle className=' cursor-pointer' color='white' fontSize='2.5em'/>
             </div>
         </div>
     </nav>
