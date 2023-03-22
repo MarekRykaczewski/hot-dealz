c0c77ace0fc0790fe2d51b1f546a599aebd061aa
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-22 18:33:16 +0100

    Implement other authentication functions

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { 
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword, 
@@ -11,12 +11,31 @@ const UserContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
 
+    const [user, setUser] = useState({})
+
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logout = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
     return (
-        <UserContext.Provider value={{ createUser }}>
+        <UserContext.Provider value={{ createUser, user, logout, signIn }}>
             {children}
         </UserContext.Provider>
     )
