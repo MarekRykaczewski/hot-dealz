0e0ffe9cc2fac1fa663cc9fbe566186c2346471d
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-23 17:36:02 +0100

    refactor layout

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { UserAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { TbPlaceholder } from 'react-icons/tb'
 
 function Settings() {
 
@@ -19,12 +21,33 @@ function Settings() {
   }
 
   return (
-    <div className='flex flex-col h-screen bg-slate-300 p-6 gap-3'>
-      <div>
-        <h1> Settings </h1>
-        <div>
-          
+    <div className='flex flex-row h-screen bg-slate-300 gap-3'>
+      <div className='p-8 w-[450px]'>
+        <h1 className='text-2xl mb-5 font-bold'> Settings </h1>
+        <div className='flex flex-col gap-3'>
+          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+            <TbPlaceholder fontSize='1.2em'/>
+            <span> Profile </span>
+          </Link>
+          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+            <TbPlaceholder fontSize='1.2em'/>
+            <span> Prefrences </span>
+          </Link>
+          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+            <TbPlaceholder fontSize='1.2em'/>
+            <span> Connect </span>
+          </Link>
+          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+            <TbPlaceholder fontSize='1.2em'/>
+            <span> Notifications </span>
+          </Link>
+          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+            <TbPlaceholder fontSize='1.2em'/>
+            <span> Subscriptions </span>
+          </Link>
         </div>
+      </div>
+      <div className='bg-white w-full'>
       </div>
     </div>
   )
