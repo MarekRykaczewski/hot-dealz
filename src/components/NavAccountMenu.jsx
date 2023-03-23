b43b77591ef9ce52a4e8897c7da224ea5b523694
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-23 17:05:16 +0100

    refactor class to className

--- src/components/NavAccountMenu.jsx
+++ src/components/NavAccountMenu.jsx
@@ -15,24 +15,24 @@ function NavAccountMenu() {
   }
 
   return (
-  <div id="dropdownInformation" class="z-10 absolute top-12 right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-      <div class="px-4 py-3 text-sm text-white bg-orange-500 rounded-t-lg">
+  <div id="dropdownInformation" className="z-10 absolute top-12 right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+      <div className="px-4 py-3 text-sm text-white bg-orange-500 rounded-t-lg">
         <div>Username</div>
-        <div class="font-medium truncate">{user && user.email}</div>
+        <div className="font-medium truncate">{user && user.email}</div>
       </div>
-      <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
+      <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
         <li>
-          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 1</a>
+          <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 1</a>
         </li>
         <li>
-          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 2</a>
+          <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 2</a>
         </li>
         <li>
-          <Link to='/account' class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
+          <Link to='/account' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
         </li>
       </ul>
-      <div class="py-2">
-        <a onClick={handleLogout} class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+      <div className="py-2">
+        <a onClick={handleLogout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
       </div>
   </div>
   )
