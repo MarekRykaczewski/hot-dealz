bc55884e697b8c1b58ff3d85192c2ff88f30b4a9
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-23 17:12:19 +0100

    styling changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,20 +11,20 @@ function Nav({toggleSignInModal, openNavAccountMenu, toggleNavAccountMenu}) {
   const { user } = UserAuth()
 
   return (
-    <nav className='flex items-center justify-between flex-wrap bg-slate-700 p-6'>
+    <nav className='flex items-center justify-between bg-slate-700 p-6'>
         <Link to='/' className="flex items-center flex-shrink-0 text-white mr-6 gap-1">
             <AiFillFire fontSize='2em' color='orange'/>
             <span className="font-semibold text-xl tracking-tight">Hot Dealz</span>
         </Link>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="flex items-center justify-between w-full flex-grow lg:flex lg:items-center lg:w-auto">
             <div className="text-sm lg:flex-grow">
-            <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-white mr-4">
+            <a className="block lg:inline-block lg:mt-0 text-slate-200 hover:text-white mr-4">
                 Place1
             </a>
-            <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-white mr-4">
+            <a className="block lg:inline-block lg:mt-0 text-slate-200 hover:text-white mr-4">
                 Place2
             </a>
-            <a className="block mt-4 lg:inline-block lg:mt-0 text-slate-200 hover:text-white">
+            <a className="block lg:inline-block lg:mt-0 text-slate-200 hover:text-white">
                 Place3
             </a>
             </div>
