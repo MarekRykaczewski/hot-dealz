a30ec6eb6ddf86804fdf997322e3b0279957cb6e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-24 15:13:22 +0100

    Settings page changes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -21,33 +21,35 @@ function Settings() {
   }
 
   return (
-    <div className='flex flex-row h-screen bg-slate-300 gap-3'>
-      <div className='p-8 w-[450px]'>
-        <h1 className='text-2xl mb-5 font-bold'> Settings </h1>
-        <div className='flex flex-col gap-3'>
-          <Link to={'profile'} className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
-            <TbPlaceholder fontSize='1.2em'/>
-            <span> Profile </span>
-          </Link>
-          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
-            <TbPlaceholder fontSize='1.2em'/>
-            <span> Prefrences </span>
-          </Link>
-          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
-            <TbPlaceholder fontSize='1.2em'/>
-            <span> Connect </span>
-          </Link>
-          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
-            <TbPlaceholder fontSize='1.2em'/>
-            <span> Notifications </span>
-          </Link>
-          <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
-            <TbPlaceholder fontSize='1.2em'/>
-            <span> Subscriptions </span>
-          </Link>
+    <div className='flex flex-row h-full bg-slate-300 gap-3'>
+      <div className='w-[1000px] h-screen'>
+        <div className='p-8 w-[350px] ml-auto'>
+          <h1 className='text-2xl mb-5 font-bold'> Settings </h1>
+          <div className='flex flex-col gap-3'>
+            <Link to={'profile'} className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+              <TbPlaceholder fontSize='1.2em'/>
+              <span> Profile </span>
+            </Link>
+            <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+              <TbPlaceholder fontSize='1.2em'/>
+              <span> Preferences </span>
+            </Link>
+            <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+              <TbPlaceholder fontSize='1.2em'/>
+              <span> Connect </span>
+            </Link>
+            <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+              <TbPlaceholder fontSize='1.2em'/>
+              <span> Notifications </span>
+            </Link>
+            <Link className='flex items-center justify-start gap-5 text-[1.2em] bg-white rounded-3xl px-5 py-2 text-center'> 
+              <TbPlaceholder fontSize='1.2em'/>
+              <span> Subscriptions </span>
+            </Link>
+          </div>
         </div>
       </div>
-      <div className='bg-white w-full'>
+      <div className='bg-white w-full p-6'>
         <Outlet />
       </div>
     </div>
