ccdb4aa980eee03e8e376ae499357f141752e4d7
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-03-31 20:41:08 +0200

    fetch categories from server and use to render items

--- src/components/CategoryCarousel.jsx
+++ src/components/CategoryCarousel.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRef } from 'react'
-
-import { BsPlugin } from 'react-icons/bs'
 import { AiFillCaretLeft } from 'react-icons/ai'
 import { AiFillCaretRight } from 'react-icons/ai'
+import { db } from "../config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function CategoryCarousel() {
 
@@ -13,53 +13,41 @@ function CategoryCarousel() {
         ref.current.scrollLeft += scrollOffset
     }
     
+    useEffect(() => {
+        const fetchData = async () => {
+            let list = []
+            try {
+                const querySnapshot = await getDocs(collection(db, "itemCategories"))
+                querySnapshot.forEach((doc) => {
+                    list.push(doc.data())
+                });
+                setCategories(list)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchData()
+    }, [])
+
+    const [categories, setCategories] = useState([])
+
+    const items = 
+        categories.map(item => {
+            return (
+                <div key={item.title} className='flex flex-none gap-1 h-10 w-[150px] items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '> 
+                {item.title}
+                </div>
+            )
+        })
+    
 
   return (
     <div className=' flex items-center justify-center bg-slate-800 mb-3'>
         <button onClick={() => scroll(-150)} id='slideLeft' className='flex item-center justify-center p-2'>
             <AiFillCaretLeft fontSize='1.5em' className=' text-slate-500'/>
         </button>
-        <div ref={ref} id='carouselContainer' className=' flex flex-row w-2/4 overflow-x-hidden scroll-smooth gap-4 p-3'>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
-            <div className=' flex gap-1 items-center justify-center bg-slate-500 rounded-lg p-2 text-white font-bold '>
-                <BsPlugin />
-                <a href="">Electronics</a>
-            </div>
+        <div ref={ref} id='carouselContainer' className='flex flex-row w-2/4 overflow-x-hidden scroll-smooth gap-4 p-3'>
+        {items}
         </div>
         <button onClick={() => scroll(150)} id='slideRight' className='flex item-center justify-center p-2'>
             <AiFillCaretRight fontSize='1.5em' className='text-slate-500'/>
