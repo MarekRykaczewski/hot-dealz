72510ae165f77b87875e96a5c375cfdb71a5aef1
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-01 15:35:58 +0200

    add new animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slidefromtop: {
+          '0%': { 'max-height': '0px' },
+          '100%': {'max-height': '100px' }
+        },
+        contract: {
+          '0%': {'max-height': '100px' },
+          '100%': {'max-height': '0px' }
+        }
+      },
+      animation: {
+        slidefromtop: 'slidefromtop 0.6s ease-in-out',
+        contract: 'contract 0.6s ease-in-out forwards'
+      },
+    },
   },
   plugins: [],
 }
