db4333faa74becb7c2937885af3f550c52f59c45
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-01 14:59:01 +0200

    fetch deals from firebase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CategoryCarousel from '../components/CategoryCarousel'
 import Tabs from '../components/Tabs'
 import DealCard from '../components/DealCard'
 import FooterNav from '../components/FooterNav'
-
+import { db } from "../config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function Home() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+        let list = []
+        try {
+            const querySnapshot = await getDocs(collection(db, "deals"))
+            querySnapshot.forEach((doc) => {
+                list.push(doc.data())
+            });
+            setDeals(list)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    fetchData()
+}, [])
+
+const [deals, setDeals] = useState([])
+
+const dealElements = 
+    deals.map(item => {
+        return (
+          <DealCard
+            key={item}
+            title={item.title}
+            upvotes={item.upvotes}
+            owner={item.owner}
+            price={item.price}
+            lastBestPrice={item.lastBestPrice}
+            description={item.description}
+            date={item.posted.toDate().toDateString()}
+            time={item.posted.toDate().toLocaleTimeString()}
+           />
+        )
+    })
 
   return (
     <div className='bg-slate-400'>
     <CategoryCarousel />
-    <div className="flex flex-col w-full gap-3 justify-center items-center">
+    <div className="flex flex-col w-full h-screen gap-3 justify-start items-center">
       <Tabs />
-      <DealCard />
-      <DealCard />
-      <DealCard />
-      <DealCard />
+      {dealElements}
       <FooterNav />
     </div>
     </div>
