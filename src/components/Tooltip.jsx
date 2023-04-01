2b028510066d6665b7a7b5bf9aadef22d03850cb
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-01 15:36:42 +0200

    implement contract (exit) animation

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -2,18 +2,24 @@ import React, { useState } from 'react'
 
 function Tooltip({ text, subtext, children }) {
 
+  const [firstClick, setFirstClick] = useState(false)
   const [visible, setVisible] = useState(false)  
     
   return (
     <div 
-        onFocus={() => setVisible(true)}
-        onBlur={() => setVisible(false)}
+        onFocus={() => {
+          setFirstClick(true)
+          setVisible(true)
+        }}
+        onBlur={() => {
+          setVisible(false)
+        }}
     >
     
     {children}
-    {visible && 
-    <div className='overflow-hidden mb-3 animate-slidefromtop'>
-      <div className={`flex flex-col bg-slate-300 rounded-xl p-3`}>
+    {firstClick &&
+    <div className={`overflow-hidden ${visible ? 'animate-slidefromtop' : 'animate-contract'}`}>
+      <div className={`flex flex-col bg-slate-300 rounded-xl p-3 mb-3`}>
           <span className='text-sm font-bold text-gray-600'>{text}</span>
           <span className='text-xs text-gray-600'>{subtext}</span>
       </div>
