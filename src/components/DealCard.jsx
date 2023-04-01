1b6a2e30fafed917127b4cb1df76b4613356ab84
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-01 14:57:51 +0200

    preparecomponent props

--- src/components/DealCard.jsx
+++ src/components/DealCard.jsx
@@ -5,7 +5,7 @@ import { BsBookmark } from 'react-icons/bs'
 import { BiCommentDetail } from 'react-icons/bi'
 import { FiExternalLink } from 'react-icons/fi'
 
-function DealCard() {
+function DealCard({ title, date, time, owner, price, lastBestPrice, upvotes, description }) {
   return (
 <div className="max-w-sm w-full lg:max-w-4xl lg:flex justify-center">
   <div className="h-48 lg:h-auto lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden bg-slate-500">
@@ -15,27 +15,30 @@ function DealCard() {
       <div className="text-sm text-gray-600 flex items-center justify-between">
         <div className='flex justify-around items-center gap-2 rounded-l-full rounded-r-full border border-black w-32 h-8 p-2 mb-2'>
           <button className='text-blue-500 font-bold text-2xl'>–</button>
-          <span className='font-bold text-lg'> 345 </span>
+          <span className='font-bold text-lg'> {upvotes} </span>
           <button className='text-orange-500 font-bold text-2xl'>+</button>          
         </div>
-        <div className='flex gap-1 items-center mb-2'>
-          <AiOutlineClockCircle />
-          <span> 1h, 40 min</span>
+        <div className='flex gap-2 items-center mb-2'>
+          <AiOutlineClockCircle size={"1.4em"} />
+          <div className='flex flex-col items-center'>
+          <span className='text-xs'> {date} </span>
+          <span className='text-xs'> {time} </span>
+          </div>
         </div>
       </div>
-      <div className="text-gray-900 font-bold text-xl mb-2">Jean Paul Gaultier Le Beau / Ultra Male, woda toaletowa, 75 ml, edt, perfumy - zbiorcza</div>
+      <div className="text-gray-900 font-bold text-xl mb-2">{title}</div>
       <div className='flex gap-3'>
-        <p className='text-orange-500 font-bold'> 123,99 zl</p>
-        <del className=' text-gray-500 font-bold'> 133,99 zl </del>
-        <p> -8% </p> 
+        <p className='text-orange-500 font-bold'> {price}</p>
+        <del className=' text-gray-500 font-bold'> {lastBestPrice} </del>
+        <p> -{Math.floor((lastBestPrice - price) / price * 100)}% </p> 
       </div>
-      <p className="text-gray-700 text-base">Super ceny w Lyko na wiele zapachów zarówno męskich jak i damskich!! Sklep legitny, potwierdzony przez perfumehub: LINK Żeby uzyskać podane ceny trzeba się zarejestrować do klubu Lyko oraz wpisać kod vouch</p>
+      <p className="text-gray-700 text-base">{description}</p>
     </div>
     <div className="flex items-center justify-between gap-5">
       <div className='flex justify-center items-center'>
         <img className="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink" />
         <div className="text-sm">
-          <p className="text-gray-900 leading-none">Marek</p>
+          <p className="text-gray-900 leading-none">{owner}</p>
         </div>      
       </div>
       <div className='flex gap-5'>
