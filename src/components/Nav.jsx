5b22c7e9c38bc21f736fe002bad9854781ab4266
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-05 17:08:39 +0200

    revert merge of NavAccountMenu into Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,34 +1,14 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 import { AiFillFire } from 'react-icons/ai'
 import { BiUserCircle } from 'react-icons/bi'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
+import NavAccountMenu from './NavAccountMenu'
 
 function Nav({toggleSignInModal, openNavAccountMenu, toggleNavAccountMenu}) {
 
-    useEffect(() => {    
-        document.addEventListener("click", handleOutsideClick, true)
-        return () => document.removeEventListener("click", handleOutsideClick, true)
-        }, [openNavAccountMenu])
-
-    const { logout, user, userData } = UserAuth()
-    const navigate = useNavigate()
-    const docRef = useRef(null)
-    const profileRef = useRef(null)
-    
-    const handleOutsideClick = (e) => {
-    if(docRef.current.contains(e.target)) {
-        return
-    } else {
-        toggleNavAccountMenu()
-    }
-    }
-    
-    const handleLogout = () => {
-    logout()
-    navigate('/')
-    }
+  const { user } = UserAuth()
 
   return (
     <nav className='flex items-center justify-between bg-slate-700 p-6'>
@@ -50,28 +30,8 @@ function Nav({toggleSignInModal, openNavAccountMenu, toggleNavAccountMenu}) {
             </div>
             <div className='flex gap-2'>
                 <div className='relative'>
-                    <BiUserCircle ref={profileRef} onClick={user ? toggleNavAccountMenu : toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2.5em' /> 
-                    {openNavAccountMenu && 
-                     <div ref={docRef} id="dropdownInformation" className="z-10 absolute top-12 right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                     <div className="px-4 py-3 text-sm text-white bg-orange-500 rounded-t-lg">
-                       <div>{userData.username}</div>
-                       <div className="font-medium truncate">{user && user.email}</div>
-                     </div>
-                     <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
-                       <li>
-                         <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 1</a>
-                       </li>
-                       <li>
-                         <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 2</a>
-                       </li>
-                       <li>
-                         <Link to='/settings/profile' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
-                       </li>
-                     </ul>
-                     <div className="py-2">
-                       <a onClick={handleLogout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                     </div>
-                 </div>}
+                    <BiUserCircle onClick={user ? toggleNavAccountMenu : toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2.5em' /> 
+                    {openNavAccountMenu && <NavAccountMenu openNavAccountMenu={openNavAccountMenu} toggleNavAccountMenu={toggleNavAccountMenu} />}
                 </div>
                 
                 <Link to='/submission'>
---
+++ src/components/NavAccountMenu.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef } from 'react'
+import { UserAuth } from '../context/AuthContext'
+import { Link, useNavigate } from 'react-router-dom'
+
+function NavAccountMenu({ openNavAccountMenu, toggleNavAccountMenu }) {
+
+  useEffect(() => {    
+    document.addEventListener("click", handleOutsideClick, true)
+    return () => document.removeEventListener("click", handleOutsideClick, true)
+  }, [openNavAccountMenu])
+
+  const docRef = useRef(null)
+
+  const handleOutsideClick = (e) => {
+    if(docRef.current.contains(e.target)) {
+      return
+    } else {
+      toggleNavAccountMenu()
+    }
+  }
+
+  const { logout, user, userData } = UserAuth()
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/')
+
+  }
+
+  return (
+  <div ref={docRef} id="dropdownInformation" className="z-10 absolute top-12 right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+      <div className="px-4 py-3 text-sm text-white bg-orange-500 rounded-t-lg">
+        <div>{userData.username}</div>
+        <div className="font-medium truncate">{user && user.email}</div>
+      </div>
+      <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
+        <li>
+          <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 1</a>
+        </li>
+        <li>
+          <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Place 2</a>
+        </li>
+        <li>
+          <Link to='/settings/profile' className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
+        </li>
+      </ul>
+      <div className="py-2">
+        <a onClick={handleLogout} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+      </div>
+  </div>
+  )
+}
+
+export default NavAccountMenu
