785f81af127f092a77b35dbd1bbc45eb4d1c994d
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-13 11:42:29 +0200

    fix percent calculation

--- src/components/PreviewDealModal.jsx
+++ src/components/PreviewDealModal.jsx
@@ -35,7 +35,7 @@ function PreviewDealModal({ toggleOpenDealPreview, title, date, time, owner, pri
                 <div className='flex gap-3'>
                     <p className='text-orange-500 font-bold'> {price}</p>
                     <del className=' text-gray-500 font-bold'> {lastBestPrice} </del>
-                    <p> -{Math.floor((lastBestPrice - price) / price * 100)}% </p> 
+                    <p> -{Math.floor((lastBestPrice - price) / lastBestPrice * 100)}% </p> 
                 </div>
                 <p className="text-gray-700 text-base">{description}</p>
                 </div>
