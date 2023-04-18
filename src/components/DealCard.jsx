c3f06098bd2128ab902c1f035ec2a3980b764bb8
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-18 12:41:42 +0200

    seperate vote tracking into new component

--- src/components/DealCard.jsx
+++ src/components/DealCard.jsx
@@ -4,125 +4,11 @@ import { AiOutlineClockCircle } from 'react-icons/ai'
 import { BsBookmark } from 'react-icons/bs'
 import { BiCommentDetail } from 'react-icons/bi'
 import { FiExternalLink } from 'react-icons/fi'
-import { addDoc, collection, deleteDoc, doc, getDocs, query, where } from 'firebase/firestore'
-import { db } from '../config/firebase'
-import { UserAuth } from '../context/AuthContext'
 import { Link } from 'react-router-dom'
+import DealCardVotes from './DealCardVotes'
 
 
 function DealCard({ postId, title, date, time, owner, price, nextBestPrice, description, dealLink }) {
-
-  // TODO REFACTOR TO AVOID REPETETIVE CODE
-
-  const { user } = UserAuth()
-  const likesRef = collection(db, "likes")
-  const likesDoc = query(likesRef, where("postId", "==", postId))
-
-  const dislikesRef = collection(db, "dislikes")
-  const dislikesDoc = query(dislikesRef, where("postId", "==", postId))
-
-  const [likes, setLikes] = useState([])
-  const [dislikes, setDislikes] = useState([])
-
-  const getLikes = async () => {
-    const data = await getDocs(likesDoc)
-    setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })))
-  }
-
-  const getDislikes = async () => {
-    const data = await getDocs(dislikesDoc)
-    setDislikes(data.docs.map((doc) => ({ userId: doc.data().userId, dislikeId: doc.id })))
-  }
-
-  const addLike = async () => {
-    try {
-      const newDoc = await addDoc(likesRef, { userId: user.uid, postId: postId})
-      if (userDisliked) {
-        deleteDislike()
-      }
-      if (user) {
-        setLikes((prev) => 
-          prev
-            ? [...prev, { userId: user.uid, likeId: newDoc.id }]
-            : [{ userId: user.uid, likeId: newDoc.id }]
-        )
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const addDislike = async () => {
-    try {
-      const newDoc = await addDoc(dislikesRef, { userId: user.uid, postId: postId})
-      if (userLiked) {
-        deleteLike()
-      }
-      if (user) {
-        setDislikes((prev) => 
-          prev
-            ? [...prev, { userId: user.uid, dislikeId: newDoc.id }]
-            : [{ userId: user.uid, dislikeId: newDoc.id }]
-        )
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const deleteLike = async () => {
-    try {
-      const likeToDeleteQuery = query(
-        likesRef,
-        where("postId", "==", postId),
-        where("userId", "==", user.uid)
-      )
-
-      const likeToDeleteData = await getDocs(likeToDeleteQuery)
-      const likeId = likeToDeleteData.docs[0].id
-      const likeToDelete = doc(db, "likes", likeId)
-      await deleteDoc(likeToDelete)
-      if (user) {
-        setLikes ((prev) =>
-          prev.filter((like) => like.likeId !== likeId)
-        )
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const deleteDislike = async () => {
-    try {
-      const dislikeToDeleteQuery = query(
-        dislikesRef,
-        where("postId", "==", postId),
-        where("userId", "==", user.uid)
-      )
-
-      const dislikeToDeleteData = await getDocs(dislikeToDeleteQuery)
-      const dislikeId = dislikeToDeleteData.docs[0].id
-      const dislikeToDelete = doc(db, "dislikes", dislikeId)
-      await deleteDoc(dislikeToDelete)
-      if (user) {
-        setDislikes ((prev) =>
-          prev.filter((like) => like.dislikeId !== dislikeId)
-        )
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const userLiked = likes.find((like) => like.userId === user.uid)
-
-  const userDisliked = dislikes.find((dislike) => dislike.userId === user.uid)
-
-  useEffect(() => {
-    getLikes()
-    getDislikes()
-  }, [])
-  
   return (
 <div className="max-w-sm w-full lg:max-w-4xl lg:flex justify-center">
   <div className="h-48 lg:h-auto lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden bg-slate-500">
@@ -130,11 +16,7 @@ function DealCard({ postId, title, date, time, owner, price, nextBestPrice, desc
   <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
     <div className="mb-8">
       <div className="text-sm text-gray-600 flex items-center justify-between">
-        <div className='flex justify-around items-center gap-2 rounded-l-full rounded-r-full border border-black w-32 h-8 p-2 mb-2'>
-          <button onClick={userDisliked ? deleteDislike : addDislike} className='text-blue-500 font-bold text-2xl'>–</button>
-          <span className='font-bold text-lg'> {likes.length - dislikes.length || 0} </span>
-          <button onClick={userLiked ? deleteLike : addLike} className='text-orange-500 font-bold text-2xl'>+</button>          
-        </div>
+        <DealCardVotes postId={postId} />
         <div className='flex gap-2 items-center mb-2'>
           <AiOutlineClockCircle size={"1.4em"} />
           <div className='flex flex-col items-center'>
---
+++ src/components/DealCardVotes.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react'
+
+import { addDoc, collection, deleteDoc, doc, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../config/firebase'
+import { UserAuth } from '../context/AuthContext'
+
+function DealCardVotes({ postId }) {
+
+    const { user } = UserAuth()
+    const likesRef = collection(db, "likes")
+    const likesDoc = query(likesRef, where("postId", "==", postId))
+  
+    const dislikesRef = collection(db, "dislikes")
+    const dislikesDoc = query(dislikesRef, where("postId", "==", postId))
+  
+    const [likes, setLikes] = useState([])
+    const [dislikes, setDislikes] = useState([])
+  
+    const getLikes = async () => {
+      const data = await getDocs(likesDoc)
+      setLikes(data.docs.map((doc) => ({ userId: doc.data().userId, likeId: doc.id })))
+    }
+  
+    const getDislikes = async () => {
+      const data = await getDocs(dislikesDoc)
+      setDislikes(data.docs.map((doc) => ({ userId: doc.data().userId, dislikeId: doc.id })))
+    }
+  
+    const addLike = async () => {
+      try {
+        const newDoc = await addDoc(likesRef, { userId: user.uid, postId: postId})
+        if (userDisliked) {
+          deleteDislike()
+        }
+        if (user) {
+          setLikes((prev) => 
+            prev
+              ? [...prev, { userId: user.uid, likeId: newDoc.id }]
+              : [{ userId: user.uid, likeId: newDoc.id }]
+          )
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  
+    const addDislike = async () => {
+      try {
+        const newDoc = await addDoc(dislikesRef, { userId: user.uid, postId: postId})
+        if (userLiked) {
+          deleteLike()
+        }
+        if (user) {
+          setDislikes((prev) => 
+            prev
+              ? [...prev, { userId: user.uid, dislikeId: newDoc.id }]
+              : [{ userId: user.uid, dislikeId: newDoc.id }]
+          )
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  
+    const deleteLike = async () => {
+      try {
+        const likeToDeleteQuery = query(
+          likesRef,
+          where("postId", "==", postId),
+          where("userId", "==", user.uid)
+        )
+  
+        const likeToDeleteData = await getDocs(likeToDeleteQuery)
+        const likeId = likeToDeleteData.docs[0].id
+        const likeToDelete = doc(db, "likes", likeId)
+        await deleteDoc(likeToDelete)
+        if (user) {
+          setLikes ((prev) =>
+            prev.filter((like) => like.likeId !== likeId)
+          )
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  
+    const deleteDislike = async () => {
+      try {
+        const dislikeToDeleteQuery = query(
+          dislikesRef,
+          where("postId", "==", postId),
+          where("userId", "==", user.uid)
+        )
+  
+        const dislikeToDeleteData = await getDocs(dislikeToDeleteQuery)
+        const dislikeId = dislikeToDeleteData.docs[0].id
+        const dislikeToDelete = doc(db, "dislikes", dislikeId)
+        await deleteDoc(dislikeToDelete)
+        if (user) {
+          setDislikes ((prev) =>
+            prev.filter((like) => like.dislikeId !== dislikeId)
+          )
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  
+    const userLiked = likes.find((like) => like.userId === user.uid)
+  
+    const userDisliked = dislikes.find((dislike) => dislike.userId === user.uid)
+  
+    useEffect(() => {
+      getLikes()
+      getDislikes()
+    }, [])
+
+  return (
+    <div className='flex justify-around items-center gap-2 rounded-l-full rounded-r-full border border-black w-32 h-8 p-2 mb-2'>
+        <button onClick={userDisliked ? deleteDislike : addDislike} className='text-blue-500 font-bold text-2xl'>–</button>
+            <span className='font-bold text-lg'> {likes.length - dislikes.length || 0} </span>
+        <button onClick={userLiked ? deleteLike : addLike} className='text-orange-500 font-bold text-2xl'>+</button>          
+    </div>
+  )
+}
+
+export default DealCardVotes
