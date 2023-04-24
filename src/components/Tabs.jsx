2cab5ecb55900f5cb4827f08829387d02ae87d54
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-24 14:08:04 +0200

    styling changes

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Tabs() {
   return (
-    <div className='flex ml-auto mr-auto justify-center bg-slate-200 w-80 w-max-7xl rounded-lg gap-3 p-2'>
-        <button className=' text-slate-500 font-bold p-2 hover:text-orange-500 transition'> Hot </button>
-        <button className='text-slate-500 font-bold p-2 hover:text-orange-500 transition'> New </button>
-        <button className='text-slate-500 font-bold p-2 hover:text-orange-500 transition'> Most Comments </button>
+    <div className='flex ml-auto mr-auto justify-start bg-white w-full gap-3 px-5 py-2 border-b-2 border-gray-300'>
+        <button className=' text-slate-600 text-lg font-bold p-2 hover:text-orange-500 transition'> Hot </button>
+        <button className='text-slate-600 text-lg font-bold p-2 hover:text-orange-600 transition'> New </button>
+        <button className='text-slate-600 text-lg font-bold p-2 hover:text-orange-500 transition'> Most Comments </button>
     </div>
   )
 }
