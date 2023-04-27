4d47b552b28977bb146c992dcbf222b1acf2b604
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-27 15:13:25 +0200

    add and use FormDescription component

--- src/pages/Submission.jsx
+++ src/pages/Submission.jsx
@@ -11,6 +11,7 @@ import { FormProvider, useForm } from 'react-hook-form'
 import FormDealLink from '../components/FormDealLink'
 import FormImageUpload from '../components/FormImageUpload'
 import FormTitle from '../components/FormTitle'
+import FormDescription from '../components/FormDescription'
 
 function Submission() {
 
@@ -144,28 +145,11 @@ function Submission() {
             TITLE_CHARACTER_LIMIT={TITLE_CHARACTER_LIMIT}
           />
 
-            <div className='flex justify-between items-center'>
-              <h3 className='text-sm font-bold text-gray-500 mb-2'> Description </h3>
-              <span className='text-xs text-gray-500'> {DESCRIPTION_CHARACTER_LIMIT - formDetails.description.length} Characters remaining </span>
-            </div>
-            
-            <Tooltip text={"Tell us about your deal"} subtext={"Include details about the product, links to any relevant info/reviews, and why you think it's a deal worth sharing"}>
-            <textarea
-              {...register("description", { 
-                required: "This is required", 
-                maxLength: {
-                  value: DESCRIPTION_CHARACTER_LIMIT,
-                  message: `Max length is ${DESCRIPTION_CHARACTER_LIMIT}`
-                } 
-              })}  
-              name='description' 
-              value={formDetails.description} 
-              onChange={handleInputChange} 
-              maxLength={DESCRIPTION_CHARACTER_LIMIT} 
-              className='resize-none border rounded-md p-3 focus:outline-orange-500 w-full h-[300px]' 
-              type="text" 
-              placeholder='Here you can describe the deal in your own words and explain to other users why it is a good deal!' />
-            </Tooltip>
+          <FormDescription 
+            handleInputChange={handleInputChange}
+            formDetails={formDetails}
+            DESCRIPTION_CHARACTER_LIMIT={DESCRIPTION_CHARACTER_LIMIT}
+          />
             
             <hr className='mb-4 mt-4'></hr>
 
