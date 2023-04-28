213d5e173c8873194d7f42c65031a7694f695358
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-04-28 13:07:13 +0200

    refactor FormPriceDetails layout

--- src/components/FormPriceDetails.jsx
+++ src/components/FormPriceDetails.jsx
@@ -7,74 +7,59 @@ function FormPriceDetails({ formDetails, handleInputChange, handleCheckChange}) 
     const { register } = useFormContext();
 
     return (
-    <div className='flex flex-col items-center justify-start'>
-        <div className='flex lg:flex-row lg:gap-8 flex-wrap gap-0 w-full justify-start items-center'>
-            <div className='flex flex-col w-max gap-1'>
-                <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="price">Price </label>
-                <Tooltip text={"Tell us the price"} subtext={"This should be the total price after any discounts"}>
-                <input 
-                    {...register("price", { required: "This is required." })} 
-                    name='price' 
-                    value={formDetails.price} 
-                    onChange={handleInputChange} 
-                    className='border rounded-md p-1 focus:outline-orange-500' 
-                    type="text" 
-                />
-                </Tooltip>
-            </div>
-
-        
-        <Tooltip text={"What was the next best price?"} subtext={"This should be the total price before any discounts"}>
-            <div className='flex flex-col w-max gap-1'>
-                <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="nextBestPrice">Next best price</label>
-                <input 
-                {...register("nextBestPrice", { required: "This is required." })} 
-                name='nextBestPrice' 
-                value={formDetails.nextBestPrice} 
-                onChange={handleInputChange} 
-                className='border rounded-md p-1 focus:outline-orange-500'
-                type="text" 
-                />
-            </div>
-        </Tooltip> 
-        </div>
-
-        <div className='flex w-full justify-start items-center gap-4'>
-            <div className='flex flex-col gap-1'>
-                <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="">Shipping cost</label>
-                <input 
-                {...register("shippingCost", { required: "This is required." })} 
-                name='shippingCost' 
-                value={formDetails.shippingCost} 
-                onChange={handleInputChange} 
-                className='border rounded-md p-1 focus:outline-orange-500' 
-                type="text" 
-                />
-                <div className='flex flex-row gap-2 justify-start items-center mt-1 mb-3'>
-                    <label className='text-sm font-bold text-gray-500' htmlFor="">Free shipping?</label>
-                    <input 
-                    {...register("freeShipping", { required: "This is required." })} 
-                    onChange={(e) => handleCheckChange(e)} 
-                    type="checkbox" 
-                    />
-                </div>
-            </div>
-
-
-        </div>
-
-        <div className='flex w-full justify-start items-center'>
-        <div className='flex flex-col gap-1'>
-            <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="">Voucher Code </label>
-            <input
-            {...register("voucherCode", { required: "This is required." })} 
-            name='voucherCode' 
-            value={formDetails.voucherCode} 
+    <div className='grid grid-cols-2 items-center gap-2'>
+                
+        <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="price">Price </label>
+        <Tooltip text={"Tell us the price"} subtext={"This should be the total price after any discounts"}>
+        <input 
+            {...register("price", { required: "This is required." })} 
+            name='price' 
+            value={formDetails.price} 
             onChange={handleInputChange} 
-            className='border rounded-md p-1 focus:outline-orange-500 mb-3' 
-            type="text" />
-            </div>
-        </div>
+            className='border rounded-md p-1 focus:outline-orange-500 w-full' 
+            type="text" 
+        />
+        </Tooltip>
+
+        <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="nextBestPrice">Next best price</label>
+        <Tooltip text={"What was the next best price?"} subtext={"This should be the total price before any discounts"}>
+        <input 
+        {...register("nextBestPrice", { required: "This is required." })} 
+        name='nextBestPrice' 
+        value={formDetails.nextBestPrice} 
+        onChange={handleInputChange} 
+        className='border rounded-md p-1 focus:outline-orange-500 w-full'
+        type="text" 
+        />
+        </Tooltip>
+
+        <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="">Shipping cost</label>
+        <input 
+        {...register("shippingCost", { required: "This is required." })} 
+        name='shippingCost' 
+        value={formDetails.shippingCost} 
+        onChange={handleInputChange} 
+        className='border rounded-md p-1 focus:outline-orange-500' 
+        type="text" 
+        />
+
+        <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="">Free shipping?</label>
+        <input 
+        {...register("freeShipping", { required: "This is required." })} 
+        onChange={(e) => handleCheckChange(e)} 
+        type="checkbox" 
+        className='scale-150'
+        />
+
+        <label className='text-sm font-bold text-gray-500 mt-1' htmlFor="">Voucher Code </label>
+        <input
+        {...register("voucherCode", { required: "This is required." })} 
+        name='voucherCode' 
+        value={formDetails.voucherCode} 
+        onChange={handleInputChange} 
+        className='border rounded-md p-1 focus:outline-orange-500 mb-3' 
+        type="text" />
+
   </div>
   )
 }
