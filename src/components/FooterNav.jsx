4fce15a981e5733bcd7b6dc2770c9078a1648a6e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-05-05 11:38:07 +0200

    merge pagination component into FooterNav

--- src/components/FooterNav.jsx
+++ src/components/FooterNav.jsx
@@ -6,13 +6,25 @@ import { AiFillCaretRight } from 'react-icons/ai'
 
 function FooterNav({ dealsPerPage, totalDeals, paginate }) {
 
-  console.log(dealsPerPage, totalDeals, paginate)
+  const pageNumbers = []
+
+  for(let i = 1; i <= Math.ceil(totalDeals / dealsPerPage); i++) {
+      pageNumbers.push(i)
+  }
 
   return (
     <div className='flex text-lg font-semibold text-slate-600 items-center justify-between p-5 bg-white border-t-2 border-gray-300 fixed bottom-0 w-full'>
         <button className='hover:text-orange-500 transition' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}> Go to top </button>
         <div>
-          <Pagination dealsPerPage={dealsPerPage} totalDeals={totalDeals} paginate={paginate}/>
+          <nav>
+          <ul className='flex gap-3 items-center justify-center'>
+              {pageNumbers.map(number => (
+                  <li key={number}>
+                      <button className='border h-8 w-8 rounded-xl hover:bg-slate-100 hover:text-orange-500 transition' onClick={() => paginate(number)}>{number}</button>                   
+                  </li>
+              ))}
+          </ul>
+          </nav>
         </div>
         <span> Show footer</span>
     </div>
