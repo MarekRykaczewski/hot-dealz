8fdcedc5670c8476c18c82370458eebebda2c1f2
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-05-10 13:46:11 +0200

    allow users to change their username

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import ProfileUsername from './ProfileUsername'
 
 import { UserAuth } from '../../context/AuthContext'
 
@@ -6,7 +7,7 @@ function Profile() {
 
     const { user, userData } = UserAuth()
 
-    const [editUsername, setEditUsername] = useState(false)
+
     const [editEmail, setEditEmail] = useState(false)
     const [editPassword, setEditPassword] = useState(false)
     const [profilePicture, setProfilePicture] = useState('')
@@ -29,16 +30,7 @@ function Profile() {
                 <input onChange={(e) => handleImageUpload(e)} className='hidden' id='profilePic' type='file' />
             </div>
         </div>
-        <div className='flex flex-row justify-start mb-10'>
-            <div className='flex flex-col mr-10 w-[200px]'>
-                <span className='font-bold'> username </span>
-            </div>
-            <div className='flex flex-col gap-4 w-[300px]'>
-                {!editUsername ? <span className='text-blue-700'> {userData.username} </span> : <input className='border p-1 rounded-lg' placeholder={userData.username} type="text" name="" id="" />}
-                <button onClick={() => setEditUsername(!editUsername)} className='self-center py-1 border rounded-2xl w-[250px] hover:bg-gray-100 hover:text-orange-500 transition'> {editUsername ? 'Cancel' : 'Change Username'} </button>
-                {editUsername && <button onClick={() => setEditUsername(false)} className='self-center py-1 border rounded-2xl w-[250px] hover:bg-gray-100 hover:text-orange-500 transition'> Submit </button>}
-            </div>
-        </div>
+        <ProfileUsername />
         <div className='flex flex-row justify-start mb-10'>
             <div className='flex flex-col mr-10 w-[200px]'>
                 <span className='font-bold'> e-mail </span>
