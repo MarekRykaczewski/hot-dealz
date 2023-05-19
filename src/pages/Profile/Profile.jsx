632149b17c5765d4b4dde9babbc53bcc44710bd9
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-05-19 19:03:57 +0200

    remove unused account data section

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -38,14 +38,6 @@ function Profile() {
         <hr className='mb-10'></hr>
         <div className='flex flex-row justify-start mb-10'>
             <div className='flex flex-col mr-10 w-[200px]'>
-                <span className='font-bold'> account data </span>
-            </div>
-            <div className='flex flex-col gap-4 w-[300px]'>
-                <button className='self-center py-1 border rounded-2xl w-[250px]'> Download data </button>
-            </div>
-        </div>
-        <div className='flex flex-row justify-start mb-10'>
-            <div className='flex flex-col mr-10 w-[200px]'>
                 <span className='font-bold'> delete account </span>
             </div>
             <div className='flex flex-col gap-4 w-[300px]'>
