4fac2cb0404166a7042eb44b87e946785d5976bf
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-05-20 19:40:38 +0200

    refactor CategoryCarousel

--- src/pages/Home/CategoryCarousel.jsx
+++ src/pages/Home/CategoryCarousel.jsx
@@ -1,49 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { useRef } from 'react'
-import { AiFillCaretLeft } from 'react-icons/ai'
-import { AiFillCaretRight } from 'react-icons/ai'
+import React, { useEffect, useState, useRef } from 'react'
+import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai'
 import { db } from "../../config/firebase";
 import { getDocs, collection } from "firebase/firestore";
 import { Link } from 'react-router-dom';
 
-function CategoryCarousel({ deals, filterDealsByCategory }) {
-
-    const ref = useRef(null)
-
-    const scroll = (scrollOffset) => {
-        ref.current.scrollLeft += scrollOffset
-    }
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            let list = []
-            try {
-                const querySnapshot = await getDocs(collection(db, "itemCategories"))
-                querySnapshot.forEach((doc) => {
-                    list.push(doc.data())
-                });
-                setCategories(list)
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        fetchData()
-    }, [])
+function CategoryCarousel() {
 
     const [categories, setCategories] = useState([])
-
-    const items = 
-        categories.map(item => {
-            return (
-                <Link key={item.title} to={`/?category=${encodeURIComponent(item.title)}`}>
-                <button className='flex flex-none text-lg gap-1 h-10 w-fit items-center justify-center bg-slate-600 rounded-lg px-5 text-white font-semibold hover:bg-slate-500 transition'> 
-                {item.title}
-                </button>
-                </Link>
-            )
-        })
     
+    const ref = useRef(null)
 
+    const scroll = (scrollOffset) => { ref.current.scrollLeft += scrollOffset }
+    
+    const fetchData = async () => {
+        let list = [];
+        try {
+          const querySnapshot = await getDocs(collection(db, "itemCategories"));
+          querySnapshot.forEach((doc) => {
+            list.push(doc.data());
+          });
+          setCategories(list);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+      const items = categories.map(item => (
+        <Link key={item.title} to={`/?category=${encodeURIComponent(item.title)}`}>
+          <button className='flex flex-none text-lg gap-1 h-10 w-fit items-center justify-center bg-slate-600 rounded-lg px-5 text-white font-semibold hover:bg-slate-500 transition'> 
+            {item.title}
+          </button>
+        </Link>
+      ));
+      
   return (
     <div className='bg-slate-900 py-2 w-full'>
         <div className='flex items-center justify-center'>
