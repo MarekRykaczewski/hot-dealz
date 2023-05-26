2abc827e7dd1a1ed462af66b56b4420b969cc2b7
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-05-26 19:29:01 +0200

    replace comment count placeholder with actual count

--- src/pages/Home/DealDetails/CommentSection.jsx
+++ src/pages/Home/DealDetails/CommentSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { setDoc, serverTimestamp, collection } from 'firebase/firestore'
+import { setDoc, serverTimestamp, collection, doc } from 'firebase/firestore'
 import { db } from '../../../config/firebase'
 import { UserAuth } from '../../../context/AuthContext'
 
@@ -28,7 +28,7 @@ function CommentSection({ postId, commentElements }) {
     <div className='flex flex-col w-full max-w-3xl bg-white mt-2 rounded-lg overflow-hidden'>
         <div className='flex flex-col gap-4 p-6'>
             <div className='flex start gap-3'>
-                <span className='text-xl'>3 Comments</span>
+                <span className='text-xl'>{commentElements.length} Comments</span>
                 <span className='text-xl'>Sorting: Newest first</span>
             </div>
             <div className='flex items-center gap-3'>
