10da53c9ca3750e42c2b84c3874517a831a88bc7
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-05-26 19:51:14 +0200

    DealDetails styling changes

--- src/pages/Home/DealDetails/DealDetails.jsx
+++ src/pages/Home/DealDetails/DealDetails.jsx
@@ -66,15 +66,17 @@ function DealDetails() {
           <div className='h-64 w-full bg-slate-500'>
             {imageCount > 0 && <ImageSlider slides={slides} />}
           </div>
-          <div className="bg-white p-4 flex flex-col w-full max-w-3xl justify-between leading rounded-lg">
-              <div className="text-sm text-gray-600 flex flex-col  justify-between">
-                <DealCardVotes postId={postId} />
-                <div className='flex gap-2 items-center mb-2'>
-                  {/* <AiOutlineClockCircle size={"1.4em"} /> */}
-                  <div className='flex flex-col items-center'>
-                  <span className='text-xs'> {date} </span>
-                  <span className='text-xs'> {time} </span>
+          <div className="bg-white p-4 flex flex-col w-full max-w-3xl">
+              <div className="text-sm text-gray-600 flex flex-col items-start gap-3">
+                  
+                <div className='flex flex-row-reverse w-full justify-between gap-2 items-center'>
+                  <div>
+                    <div className='flex flex-col items-center'>
+                    <span className='text-xs'> {date} </span>
+                    <span className='text-xs'> {time} </span>
+                    </div>
                   </div>
+                  <DealCardVotes postId={postId} />
                 </div>
               <div className="text-gray-900 font-bold text-3xl mb-2">{title}</div>
               <div className='flex gap-3 items-center'>
@@ -85,7 +87,6 @@ function DealDetails() {
               <button className='flex border hover:bg-gray-100 transition items-center justify-center rounded-full w-32 h-8'>
                 <a className='flex gap-2 items-center' href={dealLink} target='_blank'>Go to deal<FiExternalLink /> </a> 
               </button>
-              {/* <p className="text-gray-700 text-base">{description}</p> */}
               <div className="flex items-center justify-between gap-5">
                 <div className='flex justify-center items-center'>
                   <img className="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink" />
