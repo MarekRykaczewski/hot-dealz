6ae6fd10e6b3407c9ae2cc18446b65787e986551
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-05 17:10:06 +0200

    save and track comment likes

--- src/pages/Home/DealDetails/Comment.jsx
+++ src/pages/Home/DealDetails/Comment.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { collection, doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, query, where, getDocs, addDoc, deleteDoc } from "firebase/firestore";
 import { db } from "../../../config/firebase";
 import { FiThumbsUp } from 'react-icons/fi'
 import { storage } from '../../../config/firebase';
@@ -9,6 +9,7 @@ function Comment({ userId, postId, commentId, comment, date, time }) {
 
     const [username, setUsername] = useState()
     const [profileUrl, setProfileUrl] = useState()
+    const [liked, setLiked] = useState(false)
 
     async function getUsername() {
         const username = await getUsernameFromComment(commentId, postId)
@@ -55,6 +56,51 @@ function Comment({ userId, postId, commentId, comment, date, time }) {
         return username;
       }
 
+    // Function to toggle like on a comment
+    async function toggleCommentLike(dealId, commentId, userId) {
+        const likesCollectionRef = collection(db, `deals/${dealId}/comments/${commentId}/likes`);
+    
+        try {
+        // Check if the user has already liked the comment
+        const commentLikesQuery = query(likesCollectionRef, where('userId', '==', userId));
+        const commentLikesSnapshot = await getDocs(commentLikesQuery);
+        const userHasLiked = !commentLikesSnapshot.empty;
+    
+        if (userHasLiked) {
+            // User has already liked the comment, so remove the like
+            const likeDoc = commentLikesSnapshot.docs[0];
+            await deleteDoc(doc(likesCollectionRef, likeDoc.id));
+            setLiked(false)
+        } else {
+            // User has not liked the comment, so add the like
+            const newLikeDoc = await addDoc(likesCollectionRef, { userId });
+            setLiked(true)
+        }
+        } catch (error) {
+        // Handle any errors that occurred
+        console.error('Error toggling comment like:', error);
+        }
+    }
+
+    useEffect(() => {
+        // Fetch initial liked status of the comment when the component mounts
+        const fetchLikedStatus = async () => {
+          try {
+            const commentLikesQuery = query(
+              collection(db, `deals/${postId}/comments/${commentId}/likes`),
+              where('userId', '==', userId)
+            );
+            const commentLikesSnapshot = await getDocs(commentLikesQuery);
+            const userHasLiked = !commentLikesSnapshot.empty;
+            setLiked(userHasLiked);
+          } catch (error) {
+            console.error('Error fetching comment like status:', error);
+          }
+        };
+    
+        fetchLikedStatus();
+      }, [postId, commentId, userId]);
+
 
   return (
     <div className='border border-gray-400 border-t-1 border-b-0 border-l-0 border-r-0 w-full'>
@@ -68,10 +114,10 @@ function Comment({ userId, postId, commentId, comment, date, time }) {
                     </div>
                 </div>
                 <div>
-                    <div className='hover:text-orange-500 transition flex flex-row-reverse gap-2 items-center '>
-                        <span> Like</span>
-                        <FiThumbsUp />
-                    </div>
+                    <button onClick={() => toggleCommentLike(postId, commentId, userId)} className={`hover:text-orange-500 ${liked && "text-orange-500 font-bold"} cursor-pointer transition flex flex-row-reverse gap-2 items-center`}>
+                        <div> {liked ? "Liked" : "Like"}</div>
+                        <FiThumbsUp color={liked ? "orange" : "gray" } />
+                    </button>
                 </div>
             </div>
             <div>
