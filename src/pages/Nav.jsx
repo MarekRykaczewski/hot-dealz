800f49d69d89ef953eb6b5c76987b359ca416df4
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-06-07 18:20:30 +0200

    make nav responsive

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -14,25 +14,23 @@ function Nav({toggleSignInModal, openNavAccountMenu, toggleNavAccountMenu}) {
   const profileRef = useRef(null)
 
   return (
-    <nav className='flex items-center justify-between bg-slate-700 px-6 py-3'>
-        <Link to='/' className="flex items-center flex-shrink-0 text-white gap-1">
-            <AiFillFire fontSize='2.5em' color='orange'/>
-            <span className="font-semibold text-2xl tracking-tight">Hot Dealz</span>
-        </Link>
-        <div className="flex items-center justify-between w-full flex-grow lg:flex lg:items-center lg:w-auto">
-            <div className="text-sm lg:flex-grow">
-            </div>
+    <nav className='relative flex sm:flex-row flex-row sm:items-center gap-3 items-center justify-between bg-slate-700 px-6 py-3'>
+        <div className='flex flex-row flex-wrap gap-3 justify-between w-full'>
+            <Link to='/' className="flex items-center flex-shrink-0 text-white gap-1">
+                <AiFillFire fontSize='2.5em' color='orange'/>
+                <span className="font-semibold text-2xl tracking-tight">Hot Dealz</span>
+            </Link>
             <NavSearchBar />
-            <div className='flex gap-2'>
-                <div ref={profileRef} className='relative'>
-                    <BiUserCircle size={45} onClick={user ? toggleNavAccountMenu : toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2.5em' /> 
-                    {openNavAccountMenu && <NavAccountMenu profileRef={profileRef} openNavAccountMenu={openNavAccountMenu} toggleNavAccountMenu={toggleNavAccountMenu} />}
-                </div>
-                
-                <Link to='/submission'>
-                    <AiOutlinePlusCircle size={45} className=' cursor-pointer' color='white' fontSize='2.5em'/>  
-                </Link>
+        </div>
+        <div className='flex flex-col sm:flex-row gap-2'>
+            <div ref={profileRef} className='relative'>
+                <BiUserCircle size={45} onClick={user ? toggleNavAccountMenu : toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2.5em' /> 
+                {openNavAccountMenu && <NavAccountMenu profileRef={profileRef} openNavAccountMenu={openNavAccountMenu} toggleNavAccountMenu={toggleNavAccountMenu} />}
             </div>
+            
+            <Link to='/submission'>
+                <AiOutlinePlusCircle size={45} className=' cursor-pointer' color='white' fontSize='2.5em'/>  
+            </Link>
         </div>
     </nav>
   )
