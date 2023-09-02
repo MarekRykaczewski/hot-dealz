bde7747660f779eafadc441a47b35ca4901a3de9
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-09-02 17:31:07 +0200

    create AuthModal to replace sign in and sign up modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react"
 import Nav from "./pages/Nav/Nav"
 import Home from "./pages/Home/Home"
-import SignUpModal from './pages/Auth/SignUpModal'
-import SignInModal  from './pages/Auth/SignInModal'
 import { AuthContextProvider } from "./context/AuthContext"
 import { Routes, Route } from "react-router-dom"
 import Settings from "./pages/Settings/Settings"
@@ -13,24 +11,8 @@ import About from "./pages/Home/About"
 
 function App() {
 
-  const [openSignIn, setOpenSignIn] = useState(false)
-  const [openSignUp, setOpenSignUp] = useState(false)
   const [openNavAccountMenu, setOpenNavAccountMenu] = useState(false)
   
-  const toggleSignInModal = () => {
-    setOpenSignIn(!openSignIn)
-    if (openSignUp) {
-      setOpenSignUp(false)
-    }
-  }
-
-  const toggleSignUpModal = () => {
-    setOpenSignUp(!openSignUp)
-    if (openSignIn) {
-      setOpenSignIn(false)
-    }
-  }
-
   const toggleNavAccountMenu = () => {
     setOpenNavAccountMenu(!openNavAccountMenu)
   }
@@ -39,7 +21,6 @@ function App() {
   <div>
   <AuthContextProvider>
   <Nav 
-    toggleSignInModal={toggleSignInModal}
     openNavAccountMenu={openNavAccountMenu}
     toggleNavAccountMenu={toggleNavAccountMenu}
    />
@@ -52,16 +33,6 @@ function App() {
       </Route>
       <Route path='/about' element={<About />} />
     </Routes>
-  {openSignIn 
-  && <SignInModal 
-  toggleSignInModal={toggleSignInModal} 
-  toggleSignUpModal={toggleSignUpModal} 
-  />}
-  {openSignUp && 
-  <SignUpModal 
-  toggleSignUpModal={toggleSignUpModal} 
-  toggleSignInModal={toggleSignInModal} 
-  />}
   </AuthContextProvider>
   </div>
   )
--- src/pages/Nav/Nav.jsx
+++ src/pages/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 import { AiFillFire } from 'react-icons/ai'
 import { BiUserCircle } from 'react-icons/bi'
@@ -6,14 +6,18 @@ import { Link } from 'react-router-dom'
 import { UserAuth } from '../../context/AuthContext'
 import NavAccountMenu from './NavAccountMenu'
 import NavSearchBar from './NavSearchBar'
+import AuthModal from '../Auth/AuthModal'
 
-function Nav({toggleSignInModal, openNavAccountMenu, toggleNavAccountMenu}) {
+function Nav({openNavAccountMenu, toggleNavAccountMenu}) {
+
+	const [openAuthModal, setOpenAuthModal] = useState(false)
 
   const { user } = UserAuth()
 
   const profileRef = useRef(null)
 
   return (
+		<>
     <nav className='relative h-[60px] flex sm:flex-row flex-row sm:items-center gap-3 items-center justify-between bg-slate-700 px-6 py-3'>
         <div className='flex flex-row flex-wrap gap-3 justify-between w-full'>
             <Link to='/' className="flex items-center flex-shrink-0 text-white gap-1">
@@ -24,7 +28,7 @@ function Nav({toggleSignInModal, openNavAccountMenu, toggleNavAccountMenu}) {
         </div>
         <div className='flex flex-col sm:flex-row gap-2'>
             <div ref={profileRef} className='relative'>
-                <BiUserCircle size={45} onClick={user ? toggleNavAccountMenu : toggleSignInModal} className=' cursor-pointer' color='white' fontSize='2.5em' /> 
+                <BiUserCircle size={45} onClick={user ? toggleNavAccountMenu : () => setOpenAuthModal(true)} className=' cursor-pointer' color='white' fontSize='2.5em' /> 
                 {openNavAccountMenu && <NavAccountMenu profileRef={profileRef} openNavAccountMenu={openNavAccountMenu} toggleNavAccountMenu={toggleNavAccountMenu} />}
             </div>
             
@@ -33,6 +37,8 @@ function Nav({toggleSignInModal, openNavAccountMenu, toggleNavAccountMenu}) {
             </Link>
         </div>
     </nav>
+		<AuthModal open={openAuthModal} onClose={() => setOpenAuthModal(false)} />
+		</>
   )
 }
 
