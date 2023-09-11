1bb4fd225ec50c1dcce7c192fe2156fee7782f54
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-09-11 16:33:02 +0200

    add voucherCode to DealCardDetailed

--- src/components/DealCard/DealCardDetailed.jsx
+++ src/components/DealCard/DealCardDetailed.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ImageSlider from '../ImageSlider'
 import DealCardVotes from './DealCardVotes'
 import { FiExternalLink } from 'react-icons/fi'
 import { MdOutlineLocalShipping } from 'react-icons/md'
+import { BiCopyAlt } from 'react-icons/bi'
 
 const DealCardDetailed = ({ dealId, imageURLs, posted, title, price, nextBestPrice, shippingCost, dealLink, profileUrl, owner, voucherCode}) => {
 		
+	const [isCopied, setIsCopied] = useState(false)
+
+	const copyToClipboard = (e) => {
+    navigator.clipboard.writeText(e.target.value)
+    setIsCopied(true)
+
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 2000)
+  }
+
 	return (
 		<div className='bg-white p-5 flex justify-center items-center rounded-lg w-full max-w-3xl mt-3'>
-			<div className='h-64 w-full bg-slate-500 overflow-hidden rounded-xl'>
+			<div className='h-72 w-full bg-slate-500 overflow-hidden rounded-xl'>
 				<ImageSlider dealId={dealId} imageURLs={imageURLs} />
 			</div>
 			<div className="bg-white p-4 flex flex-col w-full max-w-3xl">
@@ -31,12 +43,18 @@ const DealCardDetailed = ({ dealId, imageURLs, posted, title, price, nextBestPri
 							{shippingCost}zł 
 						</p>
 					</div>
-					<button className='flex border hover:bg-gray-100 transition items-center justify-center rounded-full w-32 h-8'>
-						<a className='flex gap-2 items-center' href={dealLink} target='_blank'>
-							Go to deal
-							<FiExternalLink />
-						</a>
-					</button>
+						<div className='flex w-full gap-3 justify-center items-center'>
+						<button className='flex border hover:bg-gray-100 transition items-center justify-center rounded-full w-2/3 h-8'>
+							<a className='flex gap-2 items-center' href={dealLink} target='_blank'>
+								Go to deal
+								<FiExternalLink />
+							</a>
+						</button>
+						{voucherCode && 
+							<div className='mb-2 mt-2 flex gap-3 w-full text-gray-60'>
+								<button value={voucherCode} onClick={(e) => copyToClipboard(e)} className='flex border-dashed border-2 border-gray-300 hover:bg-gray-100 transition items-center gap-2 justify-center rounded-full w-full h-8'> {isCopied ? 'Copied!' : voucherCode} <BiCopyAlt size={20}/> </button>
+							</div>}	
+						</div>
 					<div className="flex items-center justify-between gap-5">
 						<div className='flex justify-center items-center'>
 							<img className="w-10 h-10 rounded-full mr-4" src={profileUrl} />
