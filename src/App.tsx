e568493d6c7981832865ff5e89143a0f3d09129e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-10-12 13:56:02 +0200

    configure typescript and port App component

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Nav from "./components/Nav/Nav";
+import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import { Routes, Route } from "react-router-dom";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Settings/Profile";
+import Submission from "./pages/Submission";
+
+function App() {
+  const [openNavAccountMenu, setOpenNavAccountMenu] = useState<boolean>(false); // Add the type for useState
+
+  const toggleNavAccountMenu = () => {
+    setOpenNavAccountMenu(!openNavAccountMenu);
+  }
+
+  return (
+    <div>
+      <AuthContextProvider>
+        <Nav
+          openNavAccountMenu={openNavAccountMenu}
+          toggleNavAccountMenu={toggleNavAccountMenu}
+        />
+        <Routes>
+          <Route path='*' element={<Home />} />
+          <Route path='/submission' element={<Submission />} />
+          <Route path='/settings' element={<Settings />}>
+            <Route path='/settings/profile' element={<Profile />} />
+          </Route>
+        </Routes>
+      </AuthContextProvider>
+    </div>
+  );
+}
+
+export default App;
