25d8da2dd77b5c24aae8d5296516f95fbf01397b
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-11-04 19:16:52 +0100

    remove unused export

--- src/api/firebase/firestore/index.ts
+++ src/api/firebase/firestore/index.ts
@@ -5,6 +5,5 @@ export * from "./fetchSearchResults";
 export * from "./getLikesAndDislikes";
 export * from "./handleVote";
 export * from "./submitDeal";
-export * from "./submitImages";
 export * from "./toggleSaved";
 export * from "./updateDealDetails";
