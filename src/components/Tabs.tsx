fb4d7ef91d417f99159613bcba7504baa3650272
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-07 14:29:24 +0100

    refactor Tabs

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -6,33 +6,25 @@ interface TabsProps {
 }
 
 const Tabs: React.FC<TabsProps> = ({ currentSorting, setCurrentSorting }) => {
-  const handleSortByNewest = () => {
-    setCurrentSorting("newest");
-  };
-
-  const handleSortByComments = () => {
-    setCurrentSorting("comments");
-  };
+  const tabOptions = ["newest", "comments"];
 
   return (
     <div className="bg-white w-full px-5 py-2 border-b-2 border-gray-300">
       <div className="flex justify-start gap-3 max-w-[80em] ml-auto mr-auto">
-        <button
-          className={`text-lg text-slate-800 font-bold p-2 hover:text-orange-600 transition ${
-            currentSorting === "newest" && "text-orange-600"
-          }`}
-          onClick={handleSortByNewest}
-        >
-          Newest
-        </button>
-        <button
-          className={`text-lg text-slate-800 font-bold p-2 hover:text-orange-600 transition ${
-            currentSorting === "comments" && "text-orange-600"
-          }`}
-          onClick={handleSortByComments}
-        >
-          Most Comments
-        </button>
+        {tabOptions.map((option) => (
+          <button
+            key={option}
+            className={`text-lg text-slate-800 font-bold p-2 hover:text-orange-600 transition ${
+              currentSorting === option && "text-orange-600"
+            }`}
+            onClick={() => setCurrentSorting(option)}
+            aria-selected={currentSorting === option}
+            role="tab"
+            tabIndex={currentSorting === option ? 0 : -1}
+          >
+            {option === "newest" ? "Newest" : "Most Comments"}
+          </button>
+        ))}
       </div>
     </div>
   );
