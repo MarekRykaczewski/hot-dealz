b788b2adba5a4f7cff6cbf7b01c5b602cc9518cf
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-18 16:35:28 +0100

    remove state logic

--- src/utilities/copyToClipboard.ts
+++ src/utilities/copyToClipboard.ts
@@ -1,11 +1,3 @@
-export const copyToClipboard = (
-  text: string,
-  setIsCopied: React.Dispatch<React.SetStateAction<boolean>>
-) => {
+export const copyToClipboard = (text: string) => {
   navigator.clipboard.writeText(text);
-  setIsCopied(true);
-
-  setTimeout(() => {
-    setIsCopied(false);
-  }, 2000);
 };
