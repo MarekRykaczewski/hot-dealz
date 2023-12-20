84884c4b5803b96ffad2d7a724701948f32a1a7f
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-20 20:49:01 +0100

    change SearchResultItem layout

--- src/components/Nav/SearchResultItem.tsx
+++ src/components/Nav/SearchResultItem.tsx
@@ -15,13 +15,21 @@ interface SearchResultItemProps {
 }
 
 const SearchResultItem: React.FC<SearchResultItemProps> = ({ result }) => {
+  console.log(result);
+
+  const percentOff = Math.round(
+    ((result.data.nextBestPrice - result.data.price) /
+      result.data.nextBestPrice) *
+      100
+  );
+
   return (
     <div className="w-full p-3 hover:bg-slate-200">
       <Link
         to={`/deal/${result.id}`}
         className="flex flex-row justify-start w-full"
       >
-        <div className="h-12 w-12 mr-3 border bg-white border-slate-500 p-1 rounded-lg flex items-center justify-center gap-3">
+        <div className="h-16 w-24 mr-3 bg-white p-1 rounded-lg flex items-center justify-center gap-3">
           {result.data.imageURL && (
             <img
               className="w-full h-full bg-white bg-center object-contain"
@@ -30,16 +38,22 @@ const SearchResultItem: React.FC<SearchResultItemProps> = ({ result }) => {
             />
           )}
         </div>
-        <div className="flex w-full gap-2 items-center justify-between">
-          <p className="font-bold text-slate-800 text-ellipsis">
-            <span className="text-center text-orange-500 font-bold p-1">
+        <div className="flex flex-col w-full gap-2 justify-between">
+          <div className="flex gap-1 font-semibold text-slate-800 text-ellipsis">
+            <span className="text-center text-orange-500">
               {result.data.likesCount - result.data.dislikesCount}
             </span>
-            {result.data.title}
-          </p>
-          <p className="font-bold text-sm text-orange-500">
-            {result.data.price}zł
-          </p>
+            <span>{result.data.title}</span>
+          </div>
+          <div className="flex gap-2">
+            <p className="font-semibold text-sm text-orange-500">
+              {result.data.price}zł
+            </p>
+            <p className="line-through text-sm text-gray-500">
+              {result.data.nextBestPrice}zł
+            </p>
+            <p className="text-sm text-black">-{percentOff}%</p>
+          </div>
         </div>
       </Link>
     </div>
