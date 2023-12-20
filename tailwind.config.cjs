ffb2be65f9064b03af7e52950e9caa1f826efcc4
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-20 19:14:06 +0100

    refactor config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'space-grotesk': ['"Space Grotesk"']
+        poppins: ["poppins"],
       },
       keyframes: {
         slidefromtop: {
-          '0%': { 'max-height': '0px' },
-          '100%': {'max-height': '100px' }
+          "0%": { "max-height": "0px" },
+          "100%": { "max-height": "100px" },
         },
         contract: {
-          '0%': {'max-height': '100px' },
-          '100%': {'max-height': '0px' }
-        }
+          "0%": { "max-height": "100px" },
+          "100%": { "max-height": "0px" },
+        },
       },
       animation: {
-        slidefromtop: 'slidefromtop 0.6s ease-in-out',
-        contract: 'contract 0.6s ease-in-out forwards'
+        slidefromtop: "slidefromtop 0.6s ease-in-out",
+        contract: "contract 0.6s ease-in-out forwards",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp')
-  ],
-}
+};
