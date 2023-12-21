d6ff0712446256892a8c05c44dc60c82c0affa2f
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-21 14:39:53 +0100

    change NavSearchBar layout

--- src/components/Nav/NavSearchBar.tsx
+++ src/components/Nav/NavSearchBar.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchSearchResults } from "../../api/firebase/firestore/fetchSearchResults";
 import SearchResultItem from "./SearchResultItem";
+import { FaSearch } from "react-icons/fa";
 
 interface SearchResult {
   id: string;
@@ -51,7 +52,7 @@ function NavSearchBar() {
   return (
     <div className="relative h-8 flex min-w-[300px]">
       <input
-        className="w-full rounded-full px-6 focus:outline-orange-500 focus:outline-4 focus:outline transition-all duration-100"
+        className="w-full rounded-full px-9 focus:outline-orange-500 focus:outline-4 focus:outline transition-all duration-100"
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
@@ -59,7 +60,7 @@ function NavSearchBar() {
         onBlur={() => handleSearchBlur()}
         placeholder="Search deals..."
       />
-
+      <FaSearch className="text-gray-500 absolute left-3 top-2" />
       <div className="flex flex-col items-center bg-white rounded-lg overflow-hidden absolute top-10 w-full z-50 shadow">
         {isFocused &&
           searchResults.length > 0 &&
@@ -68,9 +69,12 @@ function NavSearchBar() {
           ))}
         {isFocused && searchResults.length > 0 && (
           <div className="w-full flex justify-center mt-3 mb-3">
-            <div className="w-2/3 h-8 rounded-full text-md font-bold text-white flex items-center justify-center p-3 bg-orange-500 overflow-hidden">
-              <button onClick={handleSeeAllResults}>See all results</button>
-            </div>
+            <button
+              onClick={handleSeeAllResults}
+              className="w-2/3 h-8 rounded-full text-md font-bold text-white flex items-center justify-center p-3 bg-orange-500 overflow-hidden"
+            >
+              <span>See all results</span>
+            </button>
           </div>
         )}
       </div>
