b2a3498b64466e323cf3e208343f940cf34b648f
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-30 17:36:46 +0100

    add calculateUserStatistics

---
+++ src/api/firebase/users/calculateUserStatistics.ts
@@ -0,0 +1,45 @@
+import {
+  collection,
+  query,
+  where,
+  getCountFromServer,
+} from "firebase/firestore";
+import { db } from "../../../config/firebase";
+import getUserCommentsCount from "./getUserCommentsCount";
+import getAverageTotalScore from "../deals/getAverageTotalScore";
+import getHottestDeal from "../deals/getHottestDeal";
+
+export const calculateUserStatistics = async (userId) => {
+  try {
+    // Use the 'collection' query to count documents without fetching them
+    const dealsQuery = query(
+      collection(db, "deals"),
+      where("userId", "==", userId)
+    );
+
+    // Get the count of the result set directly
+    const countDeals = await getCountFromServer(dealsQuery);
+    const numberOfDeals = countDeals.data().count;
+
+    // Fetch the total comments for the user
+    const totalComments = await getUserCommentsCount(userId);
+
+    // Fetch the average total score for the user
+    const averageTotalScore = await getAverageTotalScore(userId);
+
+    // Fetch the hottest deal using the separate function
+    const hottestDeal = await getHottestDeal(userId);
+
+    return {
+      numberOfDeals,
+      totalComments,
+      averageTotalScore,
+      hottestDeal,
+    };
+  } catch (error) {
+    console.error("Error calculating user statistics:", error);
+    throw error;
+  }
+};
+
+export default calculateUserStatistics;
