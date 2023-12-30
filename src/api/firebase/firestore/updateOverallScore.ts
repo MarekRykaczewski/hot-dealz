cd0fd4c1cd146d406621ec86dd44b8dc11a8a84c
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-30 16:16:52 +0100

    add updateOverallScore

---
+++ src/api/firebase/firestore/updateOverallScore.ts
@@ -0,0 +1,34 @@
+import { doc, getDoc, writeBatch } from "firebase/firestore";
+import { db } from "../../../config/firebase";
+
+export async function updateOverallScore(dealId, voteType) {
+  try {
+    const dealRef = doc(db, "deals", dealId);
+    const dealDoc = await getDoc(dealRef);
+
+    if (dealDoc.exists()) {
+      const batch = writeBatch(db);
+
+      // Get the current totalScore
+      const currentScore = dealDoc.data().totalScore || 0;
+
+      // Calculate the new score based on the voteType
+      const voteValue = voteType === "like" ? 1 : -1;
+      const newScore = currentScore + voteValue;
+
+      // Update the totalScore field
+      batch.update(dealRef, { totalScore: newScore });
+
+      // Commit the batch write
+      await batch.commit();
+
+      return newScore;
+    } else {
+      console.error("Deal does not exist:", dealId);
+      return null;
+    }
+  } catch (error) {
+    console.error("Error updating overall score:", error);
+    throw error;
+  }
+}
