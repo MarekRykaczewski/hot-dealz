081713f64badafc160c61f2756015d92e6a79c0e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-12-31 22:22:36 +0100

    correct link wrapping

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -110,16 +110,17 @@ const Nav: FunctionComponent<NavProps> = ({
                   />
                 )}
               </button>
-              <button
-                className={`flex ${
-                  isFocused && "hidden sm:flex"
-                } sm:w-fit gap-0 sm:gap-1 p-2 w-10 font-semibold items-center justify-center rounded-full bg-orange-500 transition-all hover:bg-orange-400 duration-200 h-10 text-white`}
-              >
-                <Link to="/submission">
+              <Link to="/submission">
+                <button
+                  className={`flex ${
+                    isFocused && "hidden sm:flex"
+                  } sm:w-fit gap-0 sm:gap-1 p-2 w-10 font-semibold items-center justify-center rounded-full bg-orange-500 transition-all hover:bg-orange-400 duration-200 h-10 text-white`}
+                >
                   <AiOutlinePlusCircle size={30} />
-                </Link>
-                <span className="hidden sm:inline">Submit</span>
-              </button>
+
+                  <span className="hidden sm:inline">Submit</span>
+                </button>
+              </Link>
             </div>
           </div>
         </div>
