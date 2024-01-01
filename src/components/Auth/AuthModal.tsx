8df5e30f110062bc6e645bbe646cd358b8a8546d
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2024-01-01 13:42:03 +0100

    change modal styles

--- src/components/Auth/AuthModal.tsx
+++ src/components/Auth/AuthModal.tsx
@@ -48,9 +48,11 @@ function AuthModal({ open, onClose }: AuthModalProps) {
 
   return (
     <Modal open={open} onClose={onClose}>
-      <div className="flex w-[500px] gap-10">
-        <div className="flex flex-1 flex-col justify-center items-center bg-slate-800 p-6 rounded-lg text-white w-full">
-          <h3 className="text-3xl font-medium mb-4">Welcome to Hot Dealz</h3>
+      <div className="flex sm:flex-row flex-col w-full gap-3 sm:gap-6">
+        <div className="flex flex-1 flex-col justify-center items-center bg-slate-800 p-4 rounded-lg text-white w-full">
+          <h3 className="text-3xl font-medium mb-4 text-center">
+            Welcome to Hot Dealz
+          </h3>
           <p className="text-sm text-gray-500">
             {signUpMode ? "Already registered? " : "Not registered? "}
             <a
@@ -61,81 +63,84 @@ function AuthModal({ open, onClose }: AuthModalProps) {
             </a>
           </p>
         </div>
-        <div className="flex-1 w-full bg-white py-6 rounded-b-lg">
-          <form onSubmit={signUpMode ? handleSignUp : handleSignIn} action="#">
-            <h3 className="text-xl font-medium mb-4">
-              Sign {signUpMode ? "up" : "in"} to Hot Dealz
-            </h3>
+
+        <form
+          className="w-full bg-white sm:py-6 rounded-b-lg"
+          onSubmit={signUpMode ? handleSignUp : handleSignIn}
+          action="#"
+        >
+          <h3 className="text-xl font-medium mb-4">
+            Sign {signUpMode ? "up" : "in"} to Hot Dealz
+          </h3>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-900 text-sm font-medium mb-2"
+            >
+              Your email
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="name@email.com"
+              required
+            />
+          </div>
+          {signUpMode && (
             <div className="mb-4">
               <label
                 htmlFor="email"
                 className="block text-gray-900 text-sm font-medium mb-2"
               >
-                Your email
+                Your username
               </label>
               <input
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                name="email"
-                id="email"
-                placeholder="name@email.com"
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                name="username"
+                id="username"
+                placeholder="JohnSmith"
                 required
               />
             </div>
-            {signUpMode && (
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block text-gray-900 text-sm font-medium mb-2"
-                >
-                  Your username
-                </label>
-                <input
-                  onChange={(e) => setUsername(e.target.value)}
-                  type="text"
-                  name="username"
-                  id="username"
-                  placeholder="JohnSmith"
-                  required
-                />
-              </div>
-            )}
-            <div className="mb-6">
-              <label
-                htmlFor="password"
-                className="block text-gray-900 text-sm font-medium mb-2"
-              >
-                Your password
-              </label>
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                required
-              />
-            </div>
-            <div className="flex items-center justify-between mb-4">
-              <label
-                htmlFor="remember"
-                className="flex items-center text-gray-900 text-sm font-medium"
-              >
-                <input id="remember" type="checkbox" className="mr-2" />
-                Remember me
-              </label>
-              <a href="#" className="text-orange-700 text-sm hover:underline">
-                Lost Password?
-              </a>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-orange-500 hover:bg-orange-400 text-white font-medium rounded-lg py-2.5 text-sm"
+          )}
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-900 text-sm font-medium mb-2"
             >
-              {signUpMode ? "Register" : "Login to your account"}
-            </button>
-          </form>
-        </div>
+              Your password
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            <label
+              htmlFor="remember"
+              className="flex items-center text-gray-900 text-sm font-medium"
+            >
+              <input id="remember" type="checkbox" className="mr-2" />
+              Remember me
+            </label>
+            <a href="#" className="text-orange-700 text-sm hover:underline">
+              Lost Password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-orange-500 hover:bg-orange-400 text-white font-medium rounded-lg py-2.5 text-sm"
+          >
+            {signUpMode ? "Register" : "Login to your account"}
+          </button>
+        </form>
       </div>
     </Modal>
   );
