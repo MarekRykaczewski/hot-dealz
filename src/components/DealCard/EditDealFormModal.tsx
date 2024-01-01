be9719eb27b38e4f7a7870a9c839b6011bc8b551
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2024-01-01 19:54:00 +0100

    refactor EditDealFormModal

--- src/components/DealCard/EditDealFormModal.tsx
+++ src/components/DealCard/EditDealFormModal.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, Controller } from "react-hook-form";
 import Modal from "../Modal";
-import FormField from "../FormField";
+
+import {
+  titleValidation,
+  priceValidation,
+  nextBestPriceValidation,
+  shippingCostValidation,
+  dealLinkValidation,
+} from "../../utilities/validationRules";
 
 interface EditDealFormModalProps {
   isOpen: boolean;
@@ -27,73 +34,35 @@ interface FormData {
 const EditDealFormModal: React.FC<EditDealFormModalProps> = ({
   isOpen,
   onClose,
-  initialTitle = "",
-  initialPrice = 0,
-  initialNextBestPrice = 0,
-  initialShippingCost = 0,
-  initialDealLink = "",
-  initialVoucherCode = "",
+  initialTitle,
+  initialPrice,
+  initialNextBestPrice,
+  initialShippingCost,
+  initialDealLink,
+  initialVoucherCode,
   onSave,
 }: EditDealFormModalProps) => {
   const {
     handleSubmit,
     control,
+    setValue,
     formState: { errors },
-  } = useForm<FormData>({
-    defaultValues: {
-      title: initialTitle,
-      price: initialPrice,
-      nextBestPrice: initialNextBestPrice,
-      shippingCost: initialShippingCost,
-      dealLink: initialDealLink,
-      voucherCode: initialVoucherCode,
-    },
-  });
+  } = useForm<FormData>();
+
+  // Set initial values using setValue
+  React.useEffect(() => {
+    setValue("title", initialTitle || "");
+    setValue("price", initialPrice || 0);
+    setValue("nextBestPrice", initialNextBestPrice || 0);
+    setValue("shippingCost", initialShippingCost || 0);
+    setValue("dealLink", initialDealLink || "");
+    setValue("voucherCode", initialVoucherCode || "");
+  }, [initialTitle, initialDealLink, initialVoucherCode, setValue]);
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
-    const filteredData: FormData = Object.fromEntries(
-      Object.entries(data).filter(([key, value]) => value !== "")
-    );
-
-    if (Object.keys(filteredData).length > 0) {
-      onSave(filteredData);
-      onClose();
-    } else {
-      onClose();
-    }
+    onSave(data);
+    onClose();
   };
-
-  const formFields = [
-    {
-      label: "Title (at most 100 characters)",
-      name: "title",
-      maxLength: 100,
-      errorMessage: errors.title?.message,
-    },
-    {
-      label: "Price (must be a valid number)",
-      name: "price",
-      validate: (value) =>
-        !isNaN(parseFloat(value)) || "Price must be a valid number",
-      errorMessage: errors.price?.message,
-    },
-    {
-      label: "Next Best Price (optional)",
-      name: "nextBestPrice",
-      validate: (value) =>
-        !isNaN(parseFloat(value)) || "Next Best Price must be a valid number",
-      errorMessage: errors.nextBestPrice?.message,
-    },
-    {
-      label: "Shipping Cost (optional)",
-      name: "shippingCost",
-      validate: (value) =>
-        !isNaN(parseFloat(value)) || "Shipping Cost must be a valid number",
-      errorMessage: errors.shippingCost?.message,
-    },
-    { label: "Deal Link (optional)", name: "dealLink" },
-    { label: "Voucher Code (optional)", name: "voucherCode" },
-  ];
 
   return (
     <Modal open={isOpen} onClose={onClose}>
@@ -101,9 +70,179 @@ const EditDealFormModal: React.FC<EditDealFormModalProps> = ({
         Edit Deal Details
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {formFields.map((field) => (
-          <FormField key={field.name} {...field} control={control} />
-        ))}
+        <div className="mb-4">
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Title
+          </label>
+          <Controller
+            name="title"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="text"
+                  {...field}
+                  className={`mt-1 p-2 block w-full border rounded-md focus:ring-orange-500 focus:border-orange-500 ${
+                    errors.title ? "border-red-500" : ""
+                  }`}
+                />
+                {errors.title && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.title.message}
+                  </p>
+                )}
+              </>
+            )}
+            rules={titleValidation}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="price"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Price
+          </label>
+          <Controller
+            name="price"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="text"
+                  {...field}
+                  className={`mt-1 p-2 block w-full border rounded-md focus:ring-orange-500 focus:border-orange-500 ${
+                    errors.price ? "border-red-500" : ""
+                  }`}
+                />
+                {errors.price && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.price.message}
+                  </p>
+                )}
+              </>
+            )}
+            rules={priceValidation}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="nextBestPrice"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Next Best Price
+          </label>
+          <Controller
+            name="nextBestPrice"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="text"
+                  {...field}
+                  className={`mt-1 p-2 block w-full border rounded-md focus:ring-orange-500 focus:border-orange-500 ${
+                    errors.nextBestPrice ? "border-red-500" : ""
+                  }`}
+                />
+                {errors.nextBestPrice && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.nextBestPrice.message}
+                  </p>
+                )}
+              </>
+            )}
+            rules={nextBestPriceValidation}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="shippingCost"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Shipping Cost
+          </label>
+          <Controller
+            name="shippingCost"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="text"
+                  {...field}
+                  className={`mt-1 p-2 block w-full border rounded-md focus:ring-orange-500 focus:border-orange-500 ${
+                    errors.shippingCost ? "border-red-500" : ""
+                  }`}
+                />
+                {errors.shippingCost && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.shippingCost.message}
+                  </p>
+                )}
+              </>
+            )}
+            rules={shippingCostValidation}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="dealLink"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Deal Link
+          </label>
+          <Controller
+            name="dealLink"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="text"
+                  {...field}
+                  className={`mt-1 p-2 block w-full border rounded-md focus:ring-orange-500 focus:border-orange-500 ${
+                    errors.dealLink ? "border-red-500" : ""
+                  }`}
+                />
+                {errors.dealLink && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.dealLink.message}
+                  </p>
+                )}
+              </>
+            )}
+            rules={dealLinkValidation}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="voucherCode"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Voucher Code
+          </label>
+          <Controller
+            name="voucherCode"
+            control={control}
+            render={({ field }) => (
+              <>
+                <input
+                  type="text"
+                  {...field}
+                  className={`mt-1 p-2 block w-full border rounded-md focus:ring-orange-500 focus:border-orange-500 ${
+                    errors.voucherCode ? "border-red-500" : ""
+                  }`}
+                />
+                {errors.voucherCode && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.voucherCode.message}
+                  </p>
+                )}
+              </>
+            )}
+          />
+        </div>
         <div className="mt-4">
           <button
             type="submit"
