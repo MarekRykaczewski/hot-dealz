df28cd039a972eece32f1e8a45dd37df4cb0399d
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2024-01-04 21:00:24 +0100

    refactor CommentSection

--- src/components/CommentSection/index.tsx
+++ src/components/CommentSection/index.tsx
@@ -2,7 +2,11 @@ import { ChangeEvent, RefObject, useState } from "react";
 import { submitComment } from "../../api/firebase/comments";
 import { UserAuth } from "../../context/AuthContext";
 import { useCommentsData } from "../../hooks/useCommentsData";
-import { sortCommentsByLikes, sortCommentsByNewest } from "../../utils";
+import { formatDate, sortComments } from "../../utilities/commentUtils";
+import {
+  COMMENT_SORT_OPTIONS,
+  DEFAULT_PROFILE_IMAGE_URL,
+} from "../../utilities/constants";
 import CommentCard from "../CommentCard";
 
 interface CommentSectionProps {
@@ -13,12 +17,7 @@ interface CommentSectionProps {
 function CommentSection({ postId, commentInput }: CommentSectionProps) {
   const { user, userData } = UserAuth();
   const { comments, setComments } = useCommentsData(postId);
-
   const [comment, setComment] = useState<string>("");
-
-  const formatDate = (date: { seconds: number }) => {
-    return new Date(date.seconds * 1000).toLocaleString();
-  };
 
   const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setComment(e.target.value);
@@ -26,13 +25,7 @@ function CommentSection({ postId, commentInput }: CommentSectionProps) {
 
   const handleCommentSort = (event: ChangeEvent<HTMLSelectElement>) => {
     const selectedSort = event.target.value;
-
-    if (selectedSort === "Newest first") {
-      const sortedComments = sortCommentsByNewest(comments);
-      setComments(sortedComments);
-    } else if (selectedSort === "Most liked") {
-      sortCommentsByLikes(comments, setComments);
-    }
+    sortComments(comments, selectedSort, setComments);
   };
 
   const commentElements = comments.map((comment) => {
@@ -68,20 +61,26 @@ function CommentSection({ postId, commentInput }: CommentSectionProps) {
   return (
     <div className="flex flex-col w-full max-w-3xl bg-white mt-2 rounded-lg">
       <div className="flex flex-col gap-4 p-6">
-        <div className="flex start gap-3">
-          <span className="text-xl">{commentElements.length} Comments</span>
-          <label htmlFor="commentSort" className="text-xl">
-            Sorting:{" "}
-          </label>
-          <select onChange={handleCommentSort} name="commentSort">
-            <option value="Newest first">Newest first</option>
-            <option value="Most liked">Most liked</option>
-          </select>
+        <div className="flex justify-between start gap-3">
+          <span className="text-xl">{commentElements.length}Comments</span>
+          <div className="flex gap-2">
+            <label htmlFor="commentSort" className="text-xl">
+              Sorting:
+            </label>
+            <select onChange={handleCommentSort} name="commentSort">
+              <option value="Newest first">
+                {COMMENT_SORT_OPTIONS.NEWEST_FIRST}
+              </option>
+              <option value="Most liked">
+                {COMMENT_SORT_OPTIONS.MOST_LIKED}
+              </option>
+            </select>
+          </div>
         </div>
         <div className="flex items-center gap-3">
           <img
             className="self-start w-10 h-10 rounded-full mr-4"
-            src={userData.profileUrl}
+            src={userData?.profileUrl || DEFAULT_PROFILE_IMAGE_URL}
             alt="User Profile"
           />
           <textarea
