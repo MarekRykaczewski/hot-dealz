67c65fc9de8b4bbedf4bc2a28ad4433b0966e3e0
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2024-01-04 21:00:15 +0100

    create commentUtils

---
+++ src/utilities/commentUtils.ts
@@ -0,0 +1,18 @@
+import { sortCommentsByLikes, sortCommentsByNewest } from "../utils";
+
+export const sortComments = (
+  comments: Comment[],
+  selectedSort: string,
+  setComments: Function
+) => {
+  if (selectedSort === "Newest first") {
+    const sortedComments = sortCommentsByNewest(comments);
+    setComments(sortedComments);
+  } else if (selectedSort === "Most liked") {
+    sortCommentsByLikes(comments, setComments);
+  }
+};
+
+export const formatDate = (date: { seconds: number }) => {
+  return new Date(date.seconds * 1000).toLocaleString();
+};
