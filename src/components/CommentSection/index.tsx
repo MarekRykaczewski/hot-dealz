2e8d5caac40865c1e13a75190c2fa2dd47b88450
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2024-01-08 17:33:07 +0100

    fix typo

--- src/components/CommentSection/index.tsx
+++ src/components/CommentSection/index.tsx
@@ -62,7 +62,7 @@ function CommentSection({ postId, commentInput }: CommentSectionProps) {
     <div className="flex flex-col w-full max-w-3xl bg-white mt-2 rounded-lg">
       <div className="flex flex-col gap-4 p-6">
         <div className="flex justify-between start gap-3">
-          <span className="text-xl">{commentElements.length}Comments</span>
+          <span className="text-xl">{commentElements.length} Comments</span>
           <div className="flex gap-2">
             <label htmlFor="commentSort" className="text-xl">
               Sorting:
